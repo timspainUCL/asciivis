5bc734e67816e6d0a7c95bdaafc2cf48fae691c1
Tim Spain <t.spain@ucl.ac.uk>
2020-02-24 12:18:00 +0000

    Add MIT license and author information

--- include/asciivis.hpp
+++ include/asciivis.hpp
@@ -1,4 +1,20 @@
 /*
+ * Copyright 2020 University College London
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ */
+
+
+/*
  * asciivis.hpp
  *
  *	Header file for AsciiVis, a class for static ASCII-art style
@@ -20,7 +36,7 @@
  *	(i.0, j+1.0)--(i+1.0, j+1.0)
  *
  *  Created on: 21 Feb 2020
- *      Author: Tim Spain
+ *      Author: Tim Spain, t.spain@ucl.ac.uk
  */
 
 #include <ostream>
--- src/asciivis.cpp
+++ src/asciivis.cpp
@@ -1,8 +1,25 @@
 /*
+ * Copyright 2020 University College London
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ */
+
+/*
  * asciivis.cpp
  *
+ * Implementation of the AsciiVis ASCII-art visualiser.
+ *
  *  Created on: 21 Feb 2020
- *      Author: tim
+ *      Author: Tim Spain, t.spain@ucl.ac.uk
  */
 
 #include <iostream>
